73922791856036591f1ccd3be47fff685bf7fdce
Alex Anderson <alex@frontlinesms.com>
2013-09-02 14:57:32 +0300

    Began work on grails plugin.

---
+++ plugin/WsmockerGrailsPlugin.groovy
@@ -0,0 +1,8 @@
+class WsmockerGrailsPlugin {
+	def version = '0.1-SNAPSHOT'
+	def grailsVersion = '2.0 > *'
+	def title = 'Wsmocker Plugin'
+	def author = 'Alex Anderson'
+	def description = 'Grails plugin for using wsmocker to mock third-party web services in your integration and functional tests.'
+}
+
---
+++ plugin/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,31 @@
+grails.project.work.dir = 'target'
+grails.project.target.level = 1.6
+
+grails.project.dependency.resolution = {
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsHome()
+		mavenLocal()
+		grailsCentral()
+		mavenCentral()
+	}
+
+	dependencies {
+		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+	}
+
+	plugins {
+		build ':release:2.2.3',
+				':rest-client-builder:1.0.3', {
+			export = false
+		}
+		test ':spock:0.7', {
+			exclude "spock-grails-support"
+		}
+		test ':tomcat:2.2.3'
+		runtime ':hibernate:2.2.3'
+	}
+}
+
