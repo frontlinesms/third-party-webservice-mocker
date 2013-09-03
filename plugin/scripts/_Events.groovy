b62df27e833f4be1166710f8ab9177918ba81cbf
Alex Anderson <alex@frontlinesms.com>
2013-09-03 11:09:48 +0300

    Changed event to listen for proxy start in plugin from compile to testPhaseStart

--- plugin/scripts/_Events.groovy
+++ plugin/scripts/_Events.groovy
@@ -1,25 +1,29 @@
 def proxyProcess
 
-eventCompileStart = {
-	final String libName = 'wsmocker'
-	final String version = '0.1-SNAPSHOT'
-	final String libDirPath = "lib/${libName}"
-	proxyProcess = "java -jar ${baseDir}/${libDirPath}/${libName}-${version}.jar".execute()
-	try {
-		proxyProcess.inputStream.eachLine { line ->
-			println "PROXY :: $line"
-			if(line.startsWith('Server started.')) {
-				throw new RuntimeException(SERVER_STARTED_OK)
+eventTestPhaseStart = { phaseName ->
+	if(phaseName in ['functional', 'integration']) {
+		final String libName = 'wsmocker'
+		final String version = '0.1-SNAPSHOT'
+		final String libDirPath = "lib/${libName}"
+		proxyProcess = "java -jar ${baseDir}/${libDirPath}/${libName}-${version}.jar".execute()
+		try {
+			proxyProcess.inputStream.eachLine { line ->
+				println "PROXY :: $line"
+				if(line.startsWith('Server started.')) {
+					throw new RuntimeException(SERVER_STARTED_OK)
+				}
 			}
-		}
-	} catch(RuntimeException _) {
-		if(_.message != SERVER_STARTED_OK) {
-			throw _
+		} catch(RuntimeException _) {
+			if(_.message != SERVER_STARTED_OK) {
+				throw _
+			}
 		}
 	}
 }
 
 eventCompileEnd = {
-	proxyProcess?.destroy()
+	if(phaseName in ['functional', 'integration']) {
+		proxyProcess?.destroy()
+	}
 }
 
