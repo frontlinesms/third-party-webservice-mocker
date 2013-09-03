35950b31e379115aa774af45943da8cb22f59fb1
Alex Anderson <alex@frontlinesms.com>
2013-09-03 09:54:02 +0300

    Renamed plugin to make it clearer that it's different to the app.

--- plugin/WsmockerTestGrailsPlugin.groovy
+++ plugin/WsmockerTestGrailsPlugin.groovy
@@ -1,8 +1,9 @@
-class WsmockerGrailsPlugin {
+class WsmockerTestGrailsPlugin {
 	def version = '0.1-SNAPSHOT'
 	def grailsVersion = '2.0 > *'
 	def title = 'Wsmocker Plugin'
 	def author = 'Alex Anderson'
 	def description = 'Grails plugin for using wsmocker to mock third-party web services in your integration and functional tests.'
+	def license = 'APACHE'
 }
 
